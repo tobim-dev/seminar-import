34147ae382cae422eed6260ccdb8b810a61ff967
tobim-dev <mail@tmeyer.me>
2021-10-14 10:49:32 +0200

    feat - init repo and add xlsx import

---
+++ app.ts
@@ -0,0 +1,6 @@
+import XLSX from "xlsx";
+
+const workbook = XLSX.readFile("Master.xlsx");
+const sheet_name_list = workbook.SheetNames;
+const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+console.log(xlData);
