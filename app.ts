57cbb6d65f7de8ab72b694126942434e0e328179
tobim-dev <mail@tmeyer.me>
2021-10-14 11:04:43 +0200

    chore - add exel to gitignore

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import XLSX from "xlsx";
 
-const workbook = XLSX.readFile("Master.xlsx");
+const workbook = XLSX.readFile("facts.xlsx");
 const sheet_name_list = workbook.SheetNames;
 const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 console.log(xlData);
